691c15595c73a223e9dc723a8211550035dd1f48
Mathieu Dubart <mathieudubart@mini-de-mathieu.home>
2023-06-25 18:03:38 +0200

    adding/analytics => added vercel analytics

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import './assets/styles/app-style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+//inject()for analytics
+inject();
