72b0a8d5d9f4bae915e060b7984bf726a5395e86
Mathieu Dubart <mathieudubart@mini-de-mathieu.home>
2023-06-27 20:24:19 +0200

    fixing/style => fixing colors for light and dark themes

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -23,7 +23,7 @@
 <template>
   <div class="header-container">
     <h1 class="title">{{ currentProject.title }}</h1>
-    <div class="project-tags"><h3 class="subtitle"><span v-for="techno in currentProject.technos" class="single-tag rainbow">{{ techno }}</span></h3></div>
+    <div class="project-tags"><h3 class="subtitle"><span v-for="techno in currentProject.technos" class="single-tag light-grey">{{ techno }}</span></h3></div>
     <div class="see-it-live"> <a :href="currentProject.href" target="_blank" class="live-link">See it live</a></div>
     <div class="project-description">
       {{ currentProject.intro }}
