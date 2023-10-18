f3c6c7edc089022ad2c9002b8c28785cfe552061
Mathieu Dubart <mathieu.dubart@icloud.com>
2023-10-18 20:52:42 +0200

    adding/projects -> adding new projects

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -22,7 +22,7 @@
       </svg>
 
 
-    <h1 class="title">Creative web developer</h1>
+    <h1 class="title">Creative web developer, Photographer & SwiftUI lover.</h1>
     <div class="container-get-in-touch">
       <RouterLink to="/works" class="discover-work links"><h3 class="subtitle">(Discover my work)</h3></RouterLink>
     </div>
