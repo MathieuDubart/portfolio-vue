4d4a6770e86e3c129babdd6e6e4f6f482a5a3d43
Mathieu Dubart <mathieu.dubart@icloud.com>
2024-01-13 15:17:55 +0100

    fixing stuff

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -22,7 +22,7 @@
       </svg>
 
 
-    <h1 class="title">Developer, Photographer & SwiftUI lover.</h1>
+    <h1 class="title">Developer, Photographer <span class="gradient">&</span> SwiftUI lover.</h1>
     <div class="container-get-in-touch">
       <RouterLink to="/works" class="discover-work links"><h3 class="subtitle">(Discover my work)</h3></RouterLink>
     </div>
