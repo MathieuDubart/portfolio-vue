8d7c778f790106fd675d3edec3f27a857989a823
Mathieu Dubart <mathieu.dubart@icloud.com>
2024-01-20 10:57:06 +0100

    fixing/introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -22,7 +22,7 @@
       </svg>
 
 
-    <h1 class="title"><span class="decorator">Developer,</span> Photographer <span class="gradient">&</span> SwiftUI lover.</h1>
+    <h1 class="title"><span class="decorator">Developer</span> <span class="gradient">&</span> Photographer.</h1>
     <div class="container-get-in-touch">
       <RouterLink to="/works" class="discover-work links"><h3 class="subtitle">(Discover my work)</h3></RouterLink>
     </div>
