62da689eb93af693cd9591274b5bb827b54235a6
MathieuDubart <mathieu.dubart@icloud.com>
2024-01-30 10:33:41 +0100

    updating/projects -> for alt projects

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,8 +5,8 @@
 
     <h2 class="title">You can also find me here! Don't be shy, come say hi!</h2>
     <div class="networks-container">
-      <a href="https://instagram.com/mathieudbrt" target="_blank" class="links">(Instagram)</a>
       <a href="https://github.com/mathieudubart" target="_blank" class="links">(GitHub)</a>
+      <a href="https://instagram.com/mathieudbrt" target="_blank" class="links">(Instagram)</a>
       <a href="https://www.linkedin.com/in/mathieu-dubart" target="_blank" class="links">(Linkedin)</a>
     </div>
   </div>
