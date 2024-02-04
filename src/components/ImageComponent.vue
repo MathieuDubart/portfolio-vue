d4b7aefd9fbe19bc9f6d03832be3cf17cf9047c4
Mathieu Dubart <mathieu.dubart@icloud.com>
2024-02-04 12:35:41 +0100

    Debugging/bugged commit?

---
+++ src/components/ImageComponent.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    name: "ImageComponent",
+    props: {
+      source: {
+        required: true
+      }
+    }
+  };
+</script>
+
+<template>
+    <img
+    class="image-component"
+    :src="source"
+    alt="random image"
+  >
+</template>
---
+++ src/views/WorksView.vue
@@ -0,0 +1,64 @@
+<script>
+  import projectsData from "../projects-alt.json"
+
+  export default {
+  data() {
+    return {
+      projects: projectsData,
+      selectedTag: "",
+    };
+  },
+  computed: {
+    tags() {
+      // Créer un tableau unique de tous les tags
+      let tags = [];
+      this.projects.forEach((project) => {
+        project.tags.forEach((tag) => {
+          if (!tags.includes(tag)) {
+            tags.push(tag);
+          }
+        });
+      });
+      return tags;
+    },
+    filteredProjects() {
+      // Filtrer les projets en fonction du tag sélectionné
+      if (this.selectedTag === "") {
+        return this.projects;
+      } else {
+        return this.projects.filter((project) =>
+          project.tags.includes(this.selectedTag)
+        );
+      }
+    },
+  },
+};
+
+
+</script>
+
+<template>
+  <div class="works">
+    <h1 class="title"><span class="decorator">Discover</span> my work </h1>
+
+    
+    <div class="filters-container">
+      <h3 class="subtitle">Filter by</h3>
+      <select v-model="selectedTag" class="filters-select">
+        <option value="">All</option>
+        <option v-for="tag in tags" :value="tag">{{ tag }}</option>
+      </select>
+    </div>
+
+
+    <div class="works-container grid md:grid-cols-3 sm:grid-cols-1">
+      <div v-for="project in filteredProjects" :key="project.id" class="single-project">
+        <RouterLink :to="'/works'+project.route">
+          <h3 class="project-title" v-html="project.card_title"></h3>
+          <div class="project-tags"> <span v-for="tag in project.tags" class="single-tag light-grey">{{ tag }}</span> </div>
+          <div class="project-link links">See project</div>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
